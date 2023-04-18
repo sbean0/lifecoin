fbfe8a72d90a1f58460056494b92048e609c2b6b
sbean0 <34849763+sbean0@users.noreply.github.com>
2023-04-18 19:35:25 -0400

    Update BlogPage.js

--- src/BlogPage.js
+++ src/BlogPage.js
@@ -1,1 +1,32 @@
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import fs from "fs-extra";
+import Markdown from "react-markdown";
+
+function BlogPage() {
+  const [blogPost, setBlogPost] = useState("");
+  const { slug } = useParams();
+
+  useEffect(() => {
+    async function getBlogPost() {
+      try {
+        const content = await fs.readFile(`./blog/${slug}.md`, "utf8");
+        setBlogPost(content);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    getBlogPost();
+  }, [slug]);
+
+  return (
+    <div>
+      <h1>My Blog Post</h1>
+      <Markdown>{blogPost}</Markdown>
+    </div>
+  );
+}
+
+export default BlogPage;
 
